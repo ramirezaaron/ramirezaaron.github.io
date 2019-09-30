1142b561b428783d5a9b00458c8d46b4d5dd0a3c
Aarón <aaron.rc24@gmail.com>
2019-09-29 20:54:49 -0500

    Start of my personal page :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aarón Ramírez | Developer`,
+    description: `My personal page and résumé`,
+    author: `@ramirezaaron`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,22 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi </h1>
+    <p>Welcome to my page.</p>
+
+    <p>
+      I'm a <code>[web|mobile|desktop]</code> programmer. I like write code, enjoy cofee and pet cats 🐈. 
+    </p>
+    <p>
+      🚧 This site is under construction and many things will be changed. 🚧
+    </p>
+
+    <p>You can find <a href="https://twitter.com/ramirezaaron_">me</a> on Twitter</p>
   </Layout>
 )
 
