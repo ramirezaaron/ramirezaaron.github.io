443e37638aedc135a5d1cc2d9aae42933f987601
Aarón <aaron.rc24@gmail.com>
2020-01-02 23:29:20 -0600

    Agregada la página para lista de proyectos

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const IndexPage = () => (
   <Layout>
@@ -10,10 +11,15 @@ const IndexPage = () => (
     <p>Welcome to my website.</p>
 
     <p>
-      I'm a mexican <code>[web|mobile|desktop]</code> programmer. I like write code, enjoy cofee and pet cats 🐈. 
+      I'm a mexican <code>[web|mobile|desktop]</code> programmer. I like write code, enjoy cofee and pet cats 🐈 
     </p>
     <p>
-      🚧 This site is under construction and many things will be changed. 🚧
+      <span role="img" aria-label="construction">🚧</span> This site is under construction and many things will be changed. 🚧
+    </p>
+
+    <p>
+      Janaury, 2020: <br />
+      Update: <Link to="projects">Here</Link> I'll be updating the list of projects where I'm involved. 
     </p>
 
     <p>
---
+++ src/pages/projects.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Projects = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <h1>Projects</h1>
+
+    <i>
+      Coming soon...
+    </i>
+  </Layout>
+)
+
+export default Projects
