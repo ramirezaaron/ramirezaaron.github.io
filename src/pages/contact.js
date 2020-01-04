953788c9f9b4ecbb4680c8c98cf431dcf3a3703d
Aarón <aaron.rc24@gmail.com>
2020-01-04 00:35:22 -0600

    Complemento de la lista de proyectos

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contact = () => (
+  <Layout>
+    <SEO title="Contact" />
+    <h1>Contact</h1>
+  </Layout>
+)
+
+export default Contact
+
