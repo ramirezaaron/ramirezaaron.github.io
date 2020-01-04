f99478bebef8b657cb682912c4d0720ed9993461
Aarón <aaron.rc24@gmail.com>
2020-01-04 13:22:41 -0600

    Aumento de atributo width

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,8 +29,8 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
+          maxWidth: 1460,
+          padding: `50px 1.0875rem 1.45rem`,
           paddingTop: 0,
         }}
       >
